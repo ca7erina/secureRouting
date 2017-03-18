52a41837618bad4c765d8a28f32b3226458009a0
Vuong Nguyen <vuongnq.09@gmail.com>
2017-03-18 14:51:26 +0000

    Add branch aodv

--- scripts/build_ns3.sh
+++
@@ -1,3 +0,0 @@
-bake.py configure -e ns-3.26
-bake.py show
-bake.py deploy
--- scripts/install_bake.sh
+++
@@ -1,9 +0,0 @@
-hg clone http://code.nsnam.org/bake
-echo "export BAKE_HOME=`pwd`/bake" >> ~/.bashrc 
-echo "export PATH=\$PATH:\$BAKE_HOME" >> ~/.bashrc
-echo "export PYTHONPATH=\$PYTHONPATH:\$BAKE_HOME" >> ~/.bashrc
-
-source ~/.bashrc
-
-bake.py check
-
